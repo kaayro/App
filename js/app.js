e74850985f6b95510bcd27e0c763ec1815de210e
Carlos Celma <solrac@igitsoft.com>
2017-03-13 19:32:25 -0600

    barscanner test5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var app = {
 	tickets: null,
+	ready: function(){
+		document.addEventListener('deviceready',app.ready,false);
+	},
 	init:function(){
 		$('a').click(app.link);
 		$('a#ticket-take').click(qrscanner.scan);
@@ -82,7 +85,7 @@ var app = {
 		$('#touriners-profiles').show();
 	},
 };
-$(app.init);
+$(app.ready);
 
 var tickets = [
 	{id: '1',promo: 'Obtén una clase grátis en horario de 07:00pm a 09:00pm el día jueves. Este ticket es válido una sola vez por usuario de la aplicación.',address: 'Calle 7 Norte No. 8, Centro Histórico',img: 'img/promo01.JPG',location:'19.047283,-98.202763',category:'1',company:'Asere & Co.'},
--- js/barcodescan.js
+++ js/barcodescan.js
@@ -1,6 +1,5 @@
 var qrscanner = {
 	scan: function(){
-		alert(1);
 		cordova.plugins.barcodeScanner.scan(
 			function (result) {
 			  alert("We got a barcode\n" +
@@ -10,13 +9,6 @@ var qrscanner = {
 			}, 
 			function (error) {
 			  alert("Scanning failed: " + error);
-			},
-			{
-			  "preferFrontCamera" : true,
-			  "showFlipCameraButton" : true,
-			  "prompt" : "Place a barcode inside the scan area",
-			  "formats" : "QR_CODE,PDF_417", 
-			  "orientation" : "landscape"
 			}
 		);
 	}
