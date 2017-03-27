e9a1347da96483ce33d8734f535cf031f269ac9c
Carlos Celma <solrac@igitsoft.com>
2017-03-27 15:45:46 -0600

    prue

--- js/app.login.dev.js
+++ js/app.login.dev.js
@@ -3,6 +3,7 @@ app.login = {
 		if(app.login.userLogged())
 			app.login.userAccess();
 		$(document).on('submit','form#login',app.login.validatingUser);
+		$(document).on('submit','form#register-form',app.login.sendRegisterData);
 	},
 	validatingUser: function(e){
 		e.preventDefault();
@@ -18,6 +19,7 @@ app.login = {
 			data = JSON.parse(data);
 			window.localStorage.setItem('user',data.id);
 			window.localStorage.setItem('userName',data.name);
+			window.localStorage.setItem('userImg',data.img);
 			app.login.userAccess();
 		}else{
 			alert('Correo Electrónico o Contraseña incorrectos, intenta de nuevo.');//'Correo Electrónico o Contraseña incorrectos, intenta de nuevo.','Error de autentificación',null,'Aceptar'
@@ -26,11 +28,13 @@ app.login = {
 	},
 	userAccess: function(){
 		$('section#loading').show();
-		app.user = {id:window.localStorage.getItem('user'),name:window.localStorage.getItem('userName')};
+		app.user = {id:window.localStorage.getItem('user'),name:window.localStorage.getItem('userName'),img:window.localStorage.getItem('userImg')};
 		//llenar la página de profile y poner un loder hasta que llene
 		var profile = $('section#profile');
 		profile.find('.actions h4 span').text(app.user.name);
-		profile.find('article img').attr('src','http://tourindigital.com/app/imgs/'+app.user.id+'.jpg');
+		var img = app.user.id;
+		if(app.user.img == '0') img = 'default';
+		profile.find('article img').attr('src','http://tourindigital.com/app/imgs/'+img+'.jpg');
 		profile.find('article img')[0].addEventListener('load',function(){
 			app.animations.transition('#profile','quitLeft');
 			$('section#loading').hide();
@@ -39,6 +43,36 @@ app.login = {
 	userLogged: function(){
 		if(window.localStorage.getItem('user') != undefined) return true;
 		else return false;
+	},
+	sendRegisterData: function(e){
+		e.preventDefault();
+		
+		$('section#loading').show();
+		var form = $(this);
+		var data = {
+			action: 'userRegister',
+			name: form.find('input').eq(0).val(),
+			lastname: form.find('input').eq(1).val(),
+			sex: form.find('select').eq(0).val(),
+			mail: form.find('input').eq(2).val(),
+			pass: form.find('input').eq(3).val(),
+			bdate: form.find('input').eq(4).val(),
+			country: form.find('select').eq(1).val(),
+			location: 'latitud y longitud'
+		}
+		$.post(app.server+'user.php',data,app.login.registerDataResponse);
+	},
+	registerDataResponse: function(data){
+		if(data != '0' && data != 'null'){
+			data = JSON.parse(data);
+			window.localStorage.setItem('user',data.id);
+			window.localStorage.setItem('userName',data.name);
+			window.localStorage.setItem('userImg',data.img);
+			app.login.userAccess();
+		}else{
+			alert('Hubo un error al crear tu cuenta, intenta de nuevo.');//'Hubo un error al crear tu cuenta, intenta de nuevo.','Error de registro',null,'Aceptar'
+			$('section#loading').hide();
+		}
 	}
 };
 app.ready(app.login.init);
