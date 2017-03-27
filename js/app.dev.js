1c8fab548f7bc5c2549aafed32f2520bf507d82e
Carlos Celma <solrac@igitsoft.com>
2017-03-27 00:53:49 -0600

    a

--- js/app.dev.js
+++ js/app.dev.js
@@ -1,11 +1,11 @@
 var app = {
-	server: 'http://localhost/iGitWeb/TourInDigital/Server/app/',
+	server: 'http://tourindigital.com/app/',
 	user: null,
 	history: [],
 	ready: function(fnc){
 		$(function(){
-			//document.addEventListener('deviceready',fnc,false);
-			window.addEventListener('load',fnc,false);
+			document.addEventListener('deviceready',fnc,false);
+			//window.addEventListener('load',fnc,false);
 		});
 	},
 	init: function(){
