216a97378ce0570071b6b3907d442d494601026e
Carlos Celma <solrac@igitsoft.com>
2017-03-27 11:38:31 -0600

    j

--- js/app.animations.dev.js
+++ js/app.animations.dev.js
@@ -9,6 +9,9 @@ app.animations = {
 	transition: function(page,animation){
 		var currentPage = $('section.page.active');
 		var nextPage = $(page);
+		
+		if(animation == undefined) animation = 'fade';
+		
 		nextPage.addClass('preactive ' + animation);
 		currentPage.addClass(animation);
 	},
@@ -17,6 +20,22 @@ app.animations = {
 		var nextPage = $('section.preactive');
 		currentPage.attr('class','').addClass('page');
 		nextPage.attr('class','').addClass('page active');
+	},
+	transitionReverse: function(animation){
+		var result = '';
+		switch(animation){
+			case 'fade':result='fade';break;
+			case 'quitLeft':result='putRight';break;
+			case 'quitRight':result='putLeft';break;
+			case 'quitUp':result='putDown';break;
+			case 'quitDown':result='putUp';break;
+			case 'putLeft':result='quitRight';break;
+			case 'putRight':result='quitLeft';break;
+			case 'putUp':result='quitDown';break;
+			case 'putDown':result='quitUp';break;
+		}
+		
+		return result;
 	}
 };
 app.ready(app.animations.init);
--- js/app.dev.js
+++ js/app.dev.js
@@ -9,7 +9,28 @@ var app = {
 		});
 	},
 	init: function(){
-		//alert();
+		$(document).on('click','a',app.link);
+		$(document).on('click','a[data-rel=back]',app.linkBack)
+	},
+	link: function(e){
+		e.preventDefault();
+		var href = $(this).attr('href');
+		var animation = $(this).data('transition');
+		var currentPage = $('section.page.active').attr('id');
+		
+		if(href != '' && href != undefined && href != '#'){
+			app.history.push({page:'#'+currentPage,transition:animation});
+			app.animations.transition(href,animation);
+		}
+	},
+	linkBack: function(){
+		var last = app.history.length - 1;
+		var page = app.history[last].page;
+		var anim = app.history[last].transition;
+		
+		if(anim == undefined) anim = 'fade';
+		
+		app.animations.transition(page,app.animations.transitionReverse(anim));
 	}
 	
 };
