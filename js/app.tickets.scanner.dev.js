de6d7b9f8041ef6160b113cbf23307600190f072
Carlos Celma <solrac@igitsoft.com>
2017-04-07 04:33:44 -0500

    a

--- js/app.tickets.scanner.dev.js
+++ js/app.tickets.scanner.dev.js
@@ -6,7 +6,7 @@ app.tickets.scanner = {
 		$('#loading').show();
 		cordova.plugins.barcodeScanner.scan(
 			function (result) {
-			  	var arr = result.text.split(',');
+			  	var arr = result.text.split('?');
 				var data = {
 					action: 'getTicketCode',
 					table: arr[arr.length - 1],
